2ccdeb04852d4a16476e627aa610adb81e1bbf45
huynx3 <huynx3@vng.com.vn>
2022-02-07 11:23:59 +0700

    initial commit

---
+++ src/test/java/com/huy/modules/ModulesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huy.modules;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModulesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
